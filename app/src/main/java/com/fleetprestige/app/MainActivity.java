5d9027ec71f14f5c585d7c3de7f6bc2c5f607f76
Vahiff <101524724+Vahiff@users.noreply.github.com>
2026-04-19 07:53:28 +0200

    Add MainActivity

---
+++ app/src/main/java/com/fleetprestige/app/MainActivity.java
@@ -0,0 +1,26 @@
+package com.fleetprestige.app;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+}
